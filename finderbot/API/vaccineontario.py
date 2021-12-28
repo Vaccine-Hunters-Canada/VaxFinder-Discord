8914353e0a5caad9ec1744ba0d076925c53521a9
BuildTools <justin106@protonmail.com>
2021-12-28 00:02:37 -0500

    I didn't actually commit like half of that

---
+++ finderbot/API/vaccineontario.py
@@ -0,0 +1,140 @@
+from finderbot.models import *
+from finderbot.API.api import FinderAPI
+
+from google.cloud import firestore
+from google.oauth2 import service_account
+import keyring
+import pgeocode
+import os
+import time
+import json
+
+vaccineTypes = ["Pfizer", "Moderna", "J&J"]
+
+class VaccineOntarioAPI(FinderAPI):
+    def __init__(self, ratelimit=10):
+        super().__init__(ratelimit)
+        credentials = service_account.Credentials.from_service_account_file(keyring.get_password("VaxFinderDiscord", "VOKeyfile"))
+        self._database = firestore.Client(credentials=credentials)
+
+    def _get_VO_clinics(self) -> list:
+        try:
+            os.mkdir("cache")
+        except FileExistsError:
+            pass
+
+        cacheFile = None
+        for fname in os.listdir("cache"):
+            if fname.startswith("VO-"):
+                cacheFile = fname
+
+        if cacheFile:
+            creationTime = int(cacheFile.split("VO-")[1])
+            if time.time() - creationTime > 600:
+                os.remove("cache/%s" % cacheFile)
+            else:
+                with open("cache/%s" % cacheFile, "r") as f:
+                    return json.load(f)
+
+        clinics_slot_ref = self._database.collection(u"clinic").where("slots_left", "!=", {})
+        clinics_slot = [clinic.to_dict() for clinic in clinics_slot_ref.stream()]
+
+        clinics_walkin_ref = self._database.collection(u"clinic").where("walkin_times", "!=", [])
+        clinics_walkin = [clinic.to_dict() for clinic in clinics_walkin_ref.stream()]
+
+
+        clinics = clinics_slot
+        for clinic in clinics_walkin:
+            if clinic not in clinics_slot:
+                clinics.append(clinic)
+
+        with open("cache/VO-%d" % time.time(), "w") as f:
+            json.dump(clinics, f, default=str)
+        return clinics
+
+
+    def _appointments_from_postal(self, postal: str, dose=1):
+        gd = pgeocode.Nominatim("ca")
+        postalData = gd.query_postal_code(postal)
+        longitude, latitude = postalData.longitude, postalData.latitude
+        distance = 15
+
+        clinics = self._get_VO_clinics()
+        nearbyClinics = []
+
+        for clinic in clinics:
+            try:
+                clinic_coordinates = (clinic["location"]["lng"], clinic["location"]["lat"])
+            except KeyError:
+                clinic_coordinates = (0,0)
+
+            if pgeocode.haversine_distance([(longitude, latitude)], [clinic_coordinates]) < distance:
+                nearbyClinics.append(clinic)
+            elif (postal in clinic["address"]):
+                nearbyClinics.append(clinic)
+
+        locatedAppointments = []
+        for clinic in nearbyClinics:
+            try:
+                if dose != clinic["dose_number"]:
+                    continue
+            except KeyError:
+                continue
+
+            address = Region()
+            address.province = "Ontario"
+            clinicAddress = clinic["address"].split(", ")
+            address.line1 = clinicAddress[0]
+            address.city = clinicAddress[1]
+            postal = " ".join(clinicAddress[-1].split(" ")[1:])
+            if postal:
+                address.postal = postal
+            try:
+                address.longitude = clinic["location"]["lng"]
+                address.latitude = clinic["latitude"]["lat"]
+            except KeyError:
+                pass
+
+            location = Location(clinic["name"], address)
+            location.url = clinic["registration_link"]
+
+            vaccines = []
+            for vaccine in vaccineTypes:
+                if vaccine in clinic["vaccineType"]:
+                    vaccines.append(vaccine)
+
+            requirements = []
+            try:
+                for requirement in clinic["eligibility"].split("; "):
+                    requirements.append(Requirement(requirement, requirement))
+            except AttributeError:
+                pass
+
+            dates = []
+            amount_left = 1
+            try:
+                for date in clinic["slots_left"].keys():
+                    dates.append(datetime.datetime.strptime(date, "%Y-%m-%d"))
+                    amount_left += clinic["slots_left"][date]
+            except KeyError:
+                pass
+
+            walkin_dates = {}
+            try:
+                for walkin_date in clinic["walkin_times"].keys():
+                    formattedDate = datetime.datetime.strptime(walkin_date, "%Y-%m-%d")
+                    walkin_dates[formattedDate] = clinic["walkin_times"][walkin_date]
+            except KeyError:
+                pass
+
+            if not walkin_dates:
+                walkin_dates = None
+
+            appointment = VaxAppointment(location, requirements, vaccines, amount_left, dates, [dose], walkin_dates)
+            locatedAppointments.append(appointment)
+
+        return locatedAppointments
+
+
+
+
---
+++ finderbot/API/vaxfinder.py
@@ -0,0 +1,85 @@
+import requests
+import json
+
+from finderbot.models import *
+from finderbot.API.api import FinderAPI
+
+
+vaccines = ["Pfizer", "Moderna"]
+
+
+class VaxFinderAPI(FinderAPI):
+    def __init__(self, ratelimit=10):
+        super().__init__(ratelimit)
+
+    def _appointments_from_postal(self, postal: str, dose=1):
+        '''
+        Finds appointments, using the VaxFinder API, near a postal code.
+        :param postal: The first 3 letters of a postal code as a string.
+        :return: A list of VaxAppointments, or None if none are found
+        '''
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        r = requests.get("https://vax-availability-api.azurewebsites.net/api/v1/vaccine-locations"
+                            "?postal_code=%s&include_empty=false&min_date=%s" % (postal, today))
+        appointments = json.loads(r.text)
+        vaxappointments = []
+        for appointment in appointments:
+            address = Region()
+            addressdata = appointment["address"]
+            if addressdata["line1"]:
+                address.line1 = addressdata["line1"]
+            if addressdata["line2"]:
+                address.line2 = addressdata["line2"]
+            if addressdata["city"]:
+                address.city = addressdata["city"]
+            if addressdata["province"]:
+                address.province = addressdata["province"]
+            if addressdata["postcode"]:
+                address.postal = addressdata["postcode"]
+
+            loc = Location(appointment["name"], address)
+            if appointment["phone"]:
+                loc.phone = appointment["phone"]
+            if appointment["url"]:
+                loc.url = appointment["url"]
+
+            for availability in appointment["vaccineAvailabilities"]:
+                doses = [1]
+                vaccine = "Unknown"
+                if availability["tags"]:
+                    print(availability["tags"])
+                    tags = availability["tags"].split(',')
+                    if "2nd Dose" in tags:
+                        doses.append(2)
+                    if "3rd Dose" in tags:
+                        doses.append(3)
+
+                    for vax in vaccines:
+                        if vax in tags:
+                            vaccine = [vax]
+
+                    if dose not in doses:
+                        continue
+
+                requirements = []
+                reqdata = availability["requirements"]
+                for requirement in reqdata:
+                    requirements.append(Requirement(requirement["name"], requirement["description"]))
+
+                amount = availability["numberTotal"]
+                date = datetime.datetime.strptime(availability["date"].split("T")[0], "%Y-%m-%d")
+
+                # Check if this same appointment exists with another date - if so, just add the date to that
+                seen = False
+                for idx, appointment in enumerate(vaxappointments):
+                    if (appointment.requirements == requirements) and (appointment.location == loc):
+                        nappointment = appointment
+                        nappointment.dates.append(date)
+                        vaxappointments[idx] = nappointment
+                        vaxappointments[idx].amount += amount
+                        seen = True
+
+                if not seen:
+                    vaxappointments.append(VaxAppointment(loc, requirements, vaccine, amount, [date], doses))
+
+        return vaxappointments if vaxappointments else None
